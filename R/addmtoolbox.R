4b2651bd748690228c0a42ec577d618201486b8e
AlexanderFengler <alexanderfengler@gmx.de>
2015-03-02 12:05:07 -0800

    import rmarkdown

--- R/addmtoolbox.R
+++ R/addmtoolbox.R
@@ -19,7 +19,7 @@
 #'
 #' @docType package
 #' @name addmtoolbox
-#' @import data.table dplyr Rcpp RcppGSL RcppZiggurat doMC foreach iterators ggplot2 pander
+#' @import data.table dplyr Rcpp RcppGSL RcppZiggurat doMC foreach iterators ggplot2 pander rmarkdown
 #' @useDynLib addmtoolbox
 #' @importFrom Rcpp sourceCpp
 NULL
