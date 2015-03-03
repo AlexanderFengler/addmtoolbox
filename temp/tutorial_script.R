35db4116ad59c0e60c2610ea6ac83dc2b34dad4b
AlexanderFengler <alexanderfengler@gmx.de>
2015-03-03 09:20:53 -0800

    adding tutorial script

---
+++ temp/tutorial_script.R
@@ -0,0 +1,5 @@
+# Lets Prepare our data
+addm.dat  = addm_dataprep(choice.dat = addm_data_choice, eye.dat = addm_data_eye, rtbinsize = 100, timestep = 10)
+
+# Run model by trial
+addm_data_loglik_trial = addm_fit_grid(addm.dat, fit.type = 'trial')
