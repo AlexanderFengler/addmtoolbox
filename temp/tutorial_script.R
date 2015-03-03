57eb692eb82a083a8f338730b00be763e07846c2
AlexanderFengler <alexanderfengler@gmx.de>
2015-03-03 11:17:32 -0800

    refresh supplied data-sets to newest format update tutorial to show all main capabilities

--- temp/tutorial_script.R
+++ temp/tutorial_script.R
@@ -1,5 +1,57 @@
-# Lets Prepare our data
-addm.dat  = addm_dataprep(choice.dat = addm_data_choice, eye.dat = addm_data_eye, rtbinsize = 100, timestep = 10)
+# Understanding the format need for input data -----------------------------------------------
 
-# Run model by trial
-addm_data_loglik_trial = addm_fit_grid(addm.dat, fit.type = 'trial',sds = 0.07)
+# Choice Data
+View(head(addm_data_choice))
+
+# Eyetracking Data
+View(head(addm_data_eye))
+
+# --------------------------------------------------------------------------------------------
+
+# Preprocess data for easy usage with addmtoolbox --------------------------------------------
+addm.dat  = addm_dataprep(choice.dat = addm_data_choice,
+                          eye.dat = addm_data_eye,
+                          rtbinsize = 100, timestep = 10)
+
+# Look at the output
+addm.dat
+
+# View it separately
+addm.dat$choice.dat
+
+addm.dat$eye.dat
+
+addm.dat$conditions.dat
+# --------------------------------------------------------------------------------------------
+
+# Run model by trial -------------------------------------------------------------------------
+addm_data_loglik_trial = addm_fit_grid(addm.dat,
+                                       fit.type = 'trial')
+# --------------------------------------------------------------------------------------------
+
+# Plot Log likelihood ------------------------------------------------------------------------
+addm_plot_loglik(addm_data_loglik_trial)
+# --------------------------------------------------------------------------------------------
+
+# Run model by condition ---------------------------------------------------------------------
+addm_data_loglik_condition = addm_fit_grid(addm.dat,
+                                           fit.type = 'condition')
+# --------------------------------------------------------------------------------------------
+
+# Plot Log likelihood ------------------------------------------------------------------------
+addm_plot_loglik(addm_data_loglik_condition)
+# --------------------------------------------------------------------------------------------
+
+# Simulate Data
+addm_data_full_output = addm_run_by_condition(choice.dat = addm.dat$choice.dat,
+                                              conditions.dat = addm.dat$conditions.dat,
+                                              model.parameters = c(0.0015,1,0.07,0),
+                                              output.type = 'full',
+                                              nr.reps = 250)
+# --------------------------------------------------------------------------------------------
+
+# Plot some psychometrics --------------------------------------------------------------------
+addm2_plot_family(choice.dat = addm.dat$choice.dat,
+                  addm.output = addm_data_full_output)
+# --------------------------------------------------------------------------------------------
+
